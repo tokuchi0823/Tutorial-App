d29246be25358559a2558986617117a43bcc2411
ryuki <tokunagasuisan@yahoo.co.jp>
2019-03-03 17:11:38 +0000

    Finish user edit index destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,74 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
-  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
-  
   def new
-    @user = User.new # 新規作成されたUserオブジェクトをインスタンス変数に代入します
+    @user = User.new
   end
-  
-   def create
+
+  def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "ユーザーの新規作成に成功しました。"
       redirect_to @user
-      # 保存が成功した場合はここに記述した処理が実行される。
     else
       render 'new'
     end
-   end
-   
-   private
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "削除しました。"
+    redirect_to users_url
+  end
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済みユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+     # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+     # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,41 @@
 module SessionsHelper
-      # 引数に渡されたユーザーでログインする
+
+  # 引数に渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
+  # 現在ログイン中のユーザーを返す (ただし、いる場合のみ)
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
-  
-   # ユーザーがログインしていればtrue、その他ならfalseを返す
+
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-  
-   # 現在のユーザーをログアウトする
+
+  # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
   
+   # 記憶しているURL (もしくはデフォルト値) にリダイレクトする
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを記憶しておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
