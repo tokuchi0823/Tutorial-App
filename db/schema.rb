2262518a594cd3e81059832167773ec49e7ebe07
ryuki <tokunagasuisan@yahoo.co.jp>
2019-03-08 17:10:42 +0000

    Finish Attendances Show Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190304151454) do
+ActiveRecord::Schema.define(version: 20190308141458) do
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'stirng' for column 'department'
+  create_table "attendances", force: :cascade do |t|
+    t.date "worked_on"
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.string "note"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "department"
+    t.datetime "basic_time", default: "2019-02-19 22:30:00"
+    t.datetime "work_time", default: "2019-02-19 23:00:00"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
